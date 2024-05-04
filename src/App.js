daf82ef9c4f2839076484008fbb4268fd5e22a8f
JosueAbraham <rodriguez.razo.josue@gmail.com>
2024-05-04 16:29:19 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { AiOutlineUser, AiOutlineLock } from 'react-icons/ai';
 
-function App() {
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  width: 100%;
+  background-color: #3D8367;
+`;
+
+const LoginContainer = styled.div`
+  width: 350px;
+  padding: 40px;
+  background-color: #ffffff;
+  border-radius: 8px;
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const Title = styled.h1`
+  font-size: 28px;
+  margin-bottom: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: #000;
+`;
+
+const FormGroup = styled.div`
+  margin-bottom: 40px;
+  position: relative;
+`;
+
+const Label = styled.label`
+  display: block;
+  font-size: 17px;
+  margin-bottom: 20px;
+  font-weight: bold;
+  color: #000;
+`;
+
+const Input = styled.input`
+  width: calc(100% - 20px);
+  padding: 10px 10px 10px 35px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  outline: none;
+  ${({ invalid }) => invalid && `
+    border-color: red;
+  `}
+`;
+
+const Icon = styled.span`
+  position: absolute;
+  top: 60%;
+  transform: translateY(-10%);
+  right: 10px;
+  color: #000;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 12px;
+  font-size: 16px;
+  background-color: #B69840;
+  color: #fff;
+  font-weight: bold;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #A78B39;
+  }
+`;
+
+const App = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isEmailValid = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const isPasswordValid = (password) => {
+    return password.length >= 6;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <LoginContainer>
+        <Title>Iniciar Sesión</Title>
+        <form>
+          <FormGroup>
+            <Label>Email</Label>
+            <Input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Ingresa tu email"
+              invalid={email !== '' && !isEmailValid(email)}
+            />
+            <Icon><AiOutlineUser size={20} /></Icon>
+            {email !== '' && !isEmailValid(email) && <small style={{ color: 'red' }}>El correo electrónico no es válido.</small>}
+          </FormGroup>
+          <FormGroup>
+            <Label>Contraseña</Label>
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Ingresa tu contraseña"
+              invalid={password !== '' && !isPasswordValid(password)}
+            />
+            <Icon><AiOutlineLock size={20} /></Icon>
+            {password !== '' && !isPasswordValid(password) && <small style={{ color: 'red' }}>La contraseña debe tener al menos 6 caracteres.</small>}
+          </FormGroup>
+          <Button disabled={!isEmailValid(email) || !isPasswordValid(password)}>Iniciar Sesión</Button>
+        </form>
+      </LoginContainer>
+    </Container>
   );
-}
+};
 
 export default App;
